a449d57c1155058e6feaab754a926aaca17afec5
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-04-30 16:11:54 +0530

    Update Matrix Pattern 1.c

--- Matrix Pattern 1.c
+++ Matrix Pattern 1.c
@@ -5,10 +5,10 @@ int main(){
      int i,j,n;
      printf("Enter the value of n: ");
      scanf("%d", &n);
-     for(i=1 ;i<=n;i++)
+     for(i=1 ;i<=n;i++)   //Iteration for Rows
       {
            printf("\n");
-           for(j=1;j<=n;j++)
+           for(j=1;j<=n;j++) //Iteration for Columns
              {
                   printf("*");
                   printf(" ");
