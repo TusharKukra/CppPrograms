567ac81340fb01f61e20e486258208ff505ce98c
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-23 13:46:33 +0530

    Update and rename Factorial of a Number.c to Factorial of a Number.cpp

---
+++ Factorial of a Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    int r;
+    
+    if (n==0 || n==1){
+        r=1;
+    }
+    else{
+       r=n*fact(n-1);}
+    return r;
+}
+
+int main() {
+int n,r;
+cout<<"Enter any number: ";
+cin>>n;
+r=fact(n);
+cout<<"Factorial: "<<r;
+return 0;
+}
+
