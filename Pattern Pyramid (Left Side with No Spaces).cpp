fd807b1f927eb01e7a2d78bcb8cbcdbea6bcf977
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-23 23:00:38 +0530

    Create Pattern Pyramid (Left Side with No Spaces).cpp

---
+++ Pattern Pyramid (Left Side with No Spaces).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int i, j,n;
+	cout<<"Enter n: ";
+	cin>>n;
+	for(i=0; i<n; i++)
+	{
+
+		for(j=0; j<=i; j++)
+		{
+			cout<<"* ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
