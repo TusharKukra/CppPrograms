c077cb64d2d63bf84e0c1182196b64bdff437646
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-25 00:17:53 +0530

    Update Fibonacci Triangle with Digits..cpp

--- Fibonacci Triangle with Digits..cpp
+++ Fibonacci Triangle with Digits..cpp
@@ -7,7 +7,7 @@ int main()
     int i,j,a=0,b=1,c,n;
     cout<<"Enter number of Lines: ";
     cin>>n;
-    for(i=0;i<n+1;i++){
+    for(i=1;i<n+1;i++){
 
         a=0;    //These are used to print every line from 1 not from added number
         b=1;
