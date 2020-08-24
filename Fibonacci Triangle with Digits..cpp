c62efb80624dcf74ea74eb744ac295af3a74c58f
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-24 18:35:39 +0530

    Create Fibonacci Triangle with Digits..cpp

---
+++ Fibonacci Triangle with Digits..cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Fibonacci Pyramid" << endl;
+    
+    int i,j,a=0,b=1,c,n;
+    cout<<"Enter number of Lines: ";
+    cin>>n;
+    for(i=0;i<n;i++){
+
+        a=0;    //These are used to print every line from 1 not from added number
+        b=1;
+
+        for(j=0;j<i;j++){
+
+            c=a+b;
+            cout<<c<<" ";
+            a=b;
+            b=c;
+
+        }
+
+        cout<<endl;
+
+    }
+    return 0;
+}
