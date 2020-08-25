e31b06b836060817950096b2ba66865aae75397a
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-26 00:59:13 +0530

    Create Alphabet Pyramid (Non Consecutive).cpp

---
+++ Alphabet Pyramid (Non Consecutive).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int i,j,n;
+char ch = 'A';
+
+cout<<"Enter number of Lines: ";
+cin>>n;
+
+for(i=1;i<n+1;i++){
+
+    ch='A';
+
+    for(j=0;j<i;j++){
+
+        cout<<ch++<<" ";
+
+    }
+
+
+    cout<<endl;
+
+}
+
+}
