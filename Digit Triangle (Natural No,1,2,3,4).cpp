40bd521a6f651ca5317d004b2762aaf1ea53a5cb
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-25 23:43:25 +0530

    Create Digit Triangle (Natural No,1,2,3,4).cpp

---
+++ Digit Triangle (Natural No,1,2,3,4).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int i,j,a=1,n;
+
+cout<<"Enter number of Lines: ";
+cin>>n;
+
+for(i=1;i<n+1;i++){
+
+    for(j=0;j<i;j++){
+
+        cout<<a<<" ";
+        a++;
+    }
+    cout<<endl;
+}
+return 0;
+}
