50caa488334034de32386865eed6edaa709fd1ef
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-26 23:29:07 +0530

    Create Length of String.cpp

---
+++ Length of String.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{ char a[20];
+int length;
+cout<<"Enter your first Name: ";
+cin>>a;
+length=strlen(a);
+cout<<"Length of string "<< length ;
+cout<<"\nHello! "<< a;
+
+
+
+    return 0;
+}
