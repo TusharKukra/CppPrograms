2c62e220171fc0c7e5445cb72cc0d57c10e019b0
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-26 23:27:18 +0530

    Create Sort a String.cpp

---
+++ Sort a String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Enter a String: ";
+    cin>>str;
+    sort(str.begin(), str.end());
+   cout << str;
+
+    return 0;
+}
