c7ec118d946d9a596859647448a8b0533f2723b4
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-27 13:34:05 +0530

    Create Compress the String .cpp

---
+++ Compress the String .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void gen_compressed_str(string str) 
+{ 
+    int len = str.length(); 
+    for (int i = 0; i < len; i++) { 
+  
+        // Count occurrences of current character 
+        int count = 1; 
+        while (i < len - 1 && str[i] == str[i + 1]) { 
+            count++; 
+            i++; 
+        }
+        // Print character and its count
+        if (count == 1)
+        {
+          cout << str[i];
+        }
+        else
+        {
+          cout << str[i] << count;
+        }
+         
+    } 
+} 
+
+int main() {
+  
+  string str = "wwwwaaadexxxxxxywww"; 
+  gen_compressed_str(str); 
+}
