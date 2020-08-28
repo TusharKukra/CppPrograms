964f1d80c8a4fd6f727f2c52c76a47771c3e878f
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-08-29 00:08:58 +0530

    Create Bubble Sort .cpp

---
+++ Bubble Sort .cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+int n,pass,j,arr[100],temp;
+cout<<"Enter total number of elements: ";
+cin>>n;
+
+cout<<endl<<"Enter elements: ";
+for(pass=0;pass<n;pass++){
+    cin>>arr[pass];
+}
+
+cout<<"Sorting array using Bubble Sort"<<endl;
+
+for(pass=0;pass<(n-1);pass++){
+
+    for(j=0;j<(n-1-pass);j++){
+
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+
+
+    }
+}
+
+	cout<<"Sorted list in ascending order: "<<endl;
+	for(pass=0; pass<n; pass++)
+	{
+		cout<<arr[pass]<<" ";
+	}
+	return 0;
+}
