35f714a2932e60125454d24525dab0f1ed2bd362
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-13 18:07:30 +0530

    Update Maximum Sum Subarray (Cummulative Sum) .cpp

--- Array/Maximum Sum Subarray (Cummulative Sum) .cpp
+++ Array/Maximum Sum Subarray (Cummulative Sum) .cpp
@@ -1,1 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+// Maximum Subarray Sum using Cummulative Sum Method
+
+int main(){
+
+     int n,i,j;
+     cin>>n;
+     int a[n], cumSum[n]={0};
+     int currentSum=0, maximumSum=0, left=-1,right=-1;
+
+     // firstly we will enter our first element of array a
+     cin>>a[0];
+
+     // then put it in the 1st position of cummulative array
+     cumSum[0] = a[0];
+
+     // now enter the elements and place it in array a[n] from 2nd Position
+     // and simultaneously insert them in cummulative array
+     for(int i =1;i<n;i++){
+          cin>>a[i];
+          cumSum[i] = cumSum[i-1]+a[i];
+
+     }
+
+
+     // Generating Subarrays
+     for(int i=0;i<n;i++){
+          for(int j=i;j<n;j++){
+
+               //Subarray
+               currentSum = 0;
+
+               currentSum = cumSum[j] - cumSum[i-1];  
+
+               //checking for maximumSum 
+               if(currentSum > maximumSum){
+                    maximumSum = currentSum;
+                    left = i;
+                    right = j;
+               }
+          }
+     }
+
+     //Printing MaxSum
+     cout<<"Maximum Sum : "<<maximumSum<<endl;
+
+     //Printing Subarray
+     for(int k=i;k<=j;k++){
+          cout<<a[k]<<" ";
+     }
+
+
+     return 0;
+}
