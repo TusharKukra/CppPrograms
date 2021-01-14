9dda5cc524e09c87aa21c039b7c9d2c6facf38cb
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-14 19:33:17 +0530

    Update Palindromic String .cpp

--- Character Array/Palindromic String .cpp
+++ Character Array/Palindromic String .cpp
@@ -1,1 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+// Check whether a given string is a palindrome or not
+// here we use two pointer method 
+
+bool isPalindrome (char a[]){
+
+     // aabbba 
+
+     int i=0;
+     int j = strlen(a) -1 ;
+
+     while (i<j){
+          if(a[i]==a[j]){
+               i++;
+               j--;
+          }
+
+          else{
+               return false;
+          }
+     }
+
+     return true;
+}
+
+int main(){
+     
+     char a[1000];
+     cin.getline(a,1000);
+
+     if(isPalindrome(a)){
+          cout<<"Its a Palindrome";
+     }
+
+     else{
+          cout<<"Not a Palindrome";
+     }
+     return 0;
+}
