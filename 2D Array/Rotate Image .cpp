46edb4cadf2d1d2085c0f22e5f646eeb43cf234d
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-18 13:40:35 +0530

    Update Rotate Image .cpp

--- 2D Array/Rotate Image .cpp
+++ 2D Array/Rotate Image .cpp
@@ -1,1 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Rotate Image Problem : Rotating a 2D Matrix
+
+//Rotating using 2 methods : using STL, without using STL
+
+//without STL
+void rotateWithoutSTL(int a[][1000], int n){
+
+    // for doing reverse of the each rows
+    for(int row=0; row<n; row++){
+        
+        int startcol = 0;
+        int endcol = n-1;
+
+        while(startcol < endcol){
+
+            swap(a[row][startcol] , a[row][endcol]);
+            startcol++;
+            endcol--;
+        }
+    }
+
+    //for doing transpose of the matrix
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+
+            if(i<j){
+            swap(a[i][j] , a[j][i]);
+            }
+        }
+    }
+
+}
+
+
+int main(){
+
+    int a[1000][1000];
+    int n;
+    cin>>n;
+    
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    rotateWithoutSTL(a,n);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
 
