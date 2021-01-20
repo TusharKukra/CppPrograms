b89298aa3369f310c997826c65b0a27297403f18
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-20 12:49:06 +0530

    Update 2D Character Array .cpp

--- 2D Array/2D Character Array .cpp
+++ 2D Array/2D Character Array .cpp
@@ -1,1 +1,28 @@
+Example: 
+4
+A B C D
+E F G H
+I J K L
+M N O P
 
+#include<bits/stdc++.h>
+using namespace std;
+
+// 2D Character Array
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    char a = 'A';
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a++<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
