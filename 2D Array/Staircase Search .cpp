b3f06e969e5c4564b66af217d9ba015432e99f7e
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-20 12:32:11 +0530

    Update Staircase Search .cpp

--- 2D Array/Staircase Search .cpp
+++ 2D Array/Staircase Search .cpp
@@ -43,4 +43,73 @@ int main() {
 
 // Method -2 (by using Binary Search - efficient method (starting from the rightmost element))
 
+#include<bits/stdc++.h>
+using namespace std;
 
+// Searching Element in a matrix (using Divide and Conquer)
+// Time Complexity : O(n+m) 
+
+// Start from top rightmost element
+
+void search( int arr[][1000], int n, int m,  int key){
+
+    int i=0;
+    int j=m-1;
+
+    while(i>=0 && i<n && j>=0 &&j<m){
+        
+        if(arr[i][j] == key){
+
+            cout<<"Found at : ("<<i+1<<","<<j+1<<")";
+            
+
+        }
+
+        else if(arr[i][j] > key){
+            
+            // it means if our top rightmost element is greater than key value
+            // then shift to left side (left column) 
+            
+            j--;
+        }
+
+        else if(arr[i][j] < key){
+
+            // it means if our element is smaller than key value
+            // then shift to down side (down the row)
+
+            i++;
+        }
+
+
+
+    }
+
+    cout<<"Not found!!"<<endl; // if key is not present (or out of bound)
+
+
+}
+
+int main() 
+{   
+    
+    int arr[1000][1000];
+    int n,m;
+    cin>>n>>m;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    } 
+    
+    int key = 29;
+
+   search(arr, n,m, key); 
+   
+    return 0; 
+} 
+
+
+
+ 
