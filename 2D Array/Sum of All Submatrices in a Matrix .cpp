fe6fabb56f9540a0566641816fd17d64feeffa38
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-20 16:06:49 +0530

    Update Sum of All Submatrices in a Matrix .cpp

--- 2D Array/Sum of All Submatrices in a Matrix .cpp
+++ 2D Array/Sum of All Submatrices in a Matrix .cpp
@@ -1,1 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Method -1 (Brute Force -Linear Search)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Sum of all Submatrices from a given Matrix
+
+// Method -1 (Brute Force -Linear Search)
+// Time Complexity - O(n^6) [Very Bad Approach]
+
+void subarraySum (int a[1000][1000], int n){
+
+    int sum = 0;
+    //loop for finding top left element:
+    // left index
+    for(int li=0;li<n;li++){
+        for(int ji=0;ji<n;ji++){
+
+            //loop for finding bottom right element: 
+            // right index
+            for(int bi=li;bi<n;bi++){
+                for(int bj=ji;bj<n;bj++){
+
+                    //loop for finding sum in each submatrix
+                    for(int i=li ; i<=bi ;i++){
+                        for(int j=ji ; j<=bj ; j++){
+                             sum += a[i][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    cout<<sum<<endl;
+}
+
+int main(){
+
+    int a[1000][1000] = {{1,1},{1,1}};
+
+    subarraySum(a,2);
+    return 0;
+
+}
