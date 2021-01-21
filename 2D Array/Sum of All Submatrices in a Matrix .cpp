92cf722f9167c3c16309111127e77eb30e6cf28b
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-21 16:31:10 +0530

    Update Sum of All Submatrices in a Matrix .cpp

--- 2D Array/Sum of All Submatrices in a Matrix .cpp
+++ 2D Array/Sum of All Submatrices in a Matrix .cpp
@@ -51,3 +51,41 @@ int main(){
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Method -2 (Efficient Method)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Sum of all Submatrices from a given Matrix
+
+// Method -2 (Efficient Way to do it -in O(n^2))
+
+int sumOfSubmatrix(int **arr, int n, int m){
+    int sum =0 ;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            int tl = (i+1)*(j+1);
+            int br = (n-i)*(m-j);
+            sum+= (tl * br)*arr[i][j];
+        }
+    }
+    return sum;
+}
+
+int main(int argc, char const *argv[]){
+
+    int n,m;
+    cin>>n>>m;
+    int **arr = new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i] = new int[m];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<sumOfSubmatrix(arr,n,m)<<endl;
+    return 0;
+}
+
+
