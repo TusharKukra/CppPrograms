426e77cd684c67f385c8144d9c9c9591cd76926f
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-21 19:05:29 +0530

    Create 2 Submatrix Sum Query .cpp

---
+++ 2D Array/2 Submatrix Sum Query .cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Submatrix Sum Query on a Matrix
+
+// you have to find Sum of a Submatrix which has { (tli, tlj) , (bri,brj) }
+
+// Method -1 (Brute Force)
+// in each query traverse each element and add them
+// TC: O(Q * m * n)
+
+// Previous Question was on Prefix Sum Matrix.
