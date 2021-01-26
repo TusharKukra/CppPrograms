08b635df5b1b785c51209e2729f2b7d9089a2f35
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-26 22:41:04 +0530

    Update sum of all submatrix efficiently .cpp

--- 2D Array/sum of all submatrix efficiently .cpp
+++ 2D Array/sum of all submatrix efficiently .cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+// Sum of all Submatrix from a given Matrix (Efficient Method)
+
+// each element will be a part of more than 1 submatrix
+// so, in each Submatrix the element will be contributing for the Sum
+
+// so we have to find the contribution of each element in the Final Sum
+// (Just find out: Sum of the Contribution of all elements)
+
+// contribution = value of the element x no. of sybmatrices, this element will be part of
+// Contriution = arr[i][j] * no. of submatrices
+
+
 int matrixSum(int arr[][n]) 
 { 
     // Variable to store 
