d525ed2343246cd4e9c8669410353c5ff66a4768
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-01-30 21:31:57 +0530

    Update Von Neuman Program CB.cpp

--- Von Neuman Program CB.cpp
+++ Von Neuman Program CB.cpp
@@ -1,1 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void binaryToDecimal (int n){
+
+    while(n>0){
+    int answer =0;
+    int power = 1;
+    int number;
+    cin>>number;
+    
+    while(number>0){
+
+        int remainder = number % 10;
+
+        answer = answer + remainder * power;
+
+        power = power * 2;
+
+        number = number/10;
+    }
+
+    cout<<answer<<endl;
+    n--;
+   
+    }
+    
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    binaryToDecimal(n);
+
+
+    return 0;
+
+}
