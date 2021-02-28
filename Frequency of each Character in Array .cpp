8384fdde41b31e54f1cfdb40da33c72f98f385cb
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-02-28 21:56:55 +0530

    Update Frequency of each Character in Array .cpp

--- Frequency of each Character in Array .cpp
+++ Frequency of each Character in Array .cpp
@@ -50,4 +50,41 @@ int main()
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Method -2 (Efficient)
+// Method -2 (Efficient -Using Hashing)
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void prCharWithFreq(string s)
+{
+  // Store all characters and 
+  // their frequencies in dictionary
+  unordered_map<char, int> d;
+ 
+  for(char i : s)
+  {
+    d[i]++;
+  }
+ 
+  // Print characters and their 
+  // frequencies in same order 
+  // of their appearance
+  for(char i : s)
+  {
+    // Print only if this 
+    // character is not printed
+    // before
+    if(d[i] != 0)
+    {
+      cout << i << d[i] << " ";
+      d[i] = 0;
+    }
+  }
+}
+  
+// Driver Code
+int main()
+{
+  string s="geeksforgeeks";
+  prCharWithFreq(s);
+}
