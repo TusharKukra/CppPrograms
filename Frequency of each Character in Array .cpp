e3cf819235441d56aff1f067ef53610669ba25be
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-02-28 21:56:08 +0530

    Update Frequency of each Character in Array .cpp

--- Frequency of each Character in Array .cpp
+++ Frequency of each Character in Array .cpp
@@ -1,1 +1,53 @@
+// Method -1 (Basic)
 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define SIZE 26
+ 
+// function to print the character and its frequency
+// in order of its occurrence
+void printCharWithFreq(string str)
+{
+    // size of the string 'str'
+    int n = str.size();
+ 
+    // 'freq[]' implemented as hash table
+    int freq[SIZE];
+ 
+    // initialize all elements of freq[] to 0
+    memset(freq, 0, sizeof(freq));
+ 
+    // accumulate freqeuncy of each character in 'str'
+    for (int i = 0; i < n; i++)
+        freq[str[i] - 'a']++;
+ 
+    // traverse 'str' from left to right
+    for (int i = 0; i < n; i++) {
+ 
+        // if frequency of character str[i] is not
+        // equal to 0
+        if (freq[str[i] - 'a'] != 0) {
+ 
+            // print the character along with its
+            // frequency
+            cout << str[i] << freq[str[i] - 'a'] << " ";
+ 
+            // update frequency of str[i] to 0 so 
+            // that the same character is not printed 
+            // again
+            freq[str[i] - 'a'] = 0;
+        }
+    }
+}
+ 
+int main()
+{
+    string str = "geeksforgeeks";
+    printCharWithFreq(str);
+    return 0;
+}
+
+
+
+// Method -2 (Efficient)
