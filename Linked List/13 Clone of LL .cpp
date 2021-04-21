3aa400a6df3c660bd26d2c6cbb20f75677464503
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-21 20:37:58 +0530

    Create 13 Clone of LL .cpp

---
+++ Linked List/13 Clone of LL .cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/*
+Given a LL having two pointers in each node.
+The first one points to the next node to the list, however,
+the other pointer is random and can point to any node to the list.
+
+Clone a Linked List 
+
+Note: size of LL is greater than 0.
+*/
+
+int main(){
+
+
+
+    return 0;
+}
