20db629bae95ceba2a3c65d3f4c0a74e60a6a0dc
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-22 22:19:19 +0530

    Update and rename 14  .cpp to 14 Mid Point of LL .cpp

---
+++ Linked List/14 Mid Point of LL .cpp
@@ -0,0 +1,110 @@
+// Linked List
+
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+	int data;
+	node* next;
+
+	node(int d) {
+		data = d;
+		next = NULL;
+	}
+};
+
+int lengthLL(node* head) {
+	int cnt = 0;
+	while (head) {
+		cnt++;
+		head = head->next;
+	}
+	return cnt;
+}
+
+void insertAtEnd(node* &head, int d) {
+	node* n = new node(d);
+	if (head == NULL) {
+		head = n;
+	}
+	else {
+		node* temp = head;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = n;
+	}
+}
+
+void print(node* head) {
+	while (head != NULL) {
+		cout << head->data << "-->";
+		head = head->next;
+	}
+	cout << "NULL" << endl;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Finding Mid using Tortoise method
+node* mid(node* head) {
+	if (head == NULL || head->next == NULL) {
+		return head;
+	}
+
+	node* slow = head;
+	node* fast = head -> next;
+	while (fast != NULL and fast -> next != NULL) {
+		fast = fast -> next -> next;
+		slow = slow -> next;
+	}
+	return slow;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void ReverseLL(node* &head) {
+	node* previous = NULL;
+    node* current = head;
+    node* nextPointer;
+
+	while (current != NULL) {
+        // firstly initialize the nextPointer with the 
+		nextPointer = current -> next;
+
+        // then change the current pointer (shift back) to previous node
+		current -> next = previous;
+
+        // now move forward all 3 pointers 1 step
+		previous = current;
+		current = nextPointer;
+	}
+	head = previous;
+}
+
+
+int main() {
+	node* head, *head1;
+	head = head1 = NULL;
+
+	insertAtEnd(head, 10);
+	insertAtEnd(head, 3);
+	insertAtEnd(head, 5);
+	insertAtEnd(head, 60);
+	insertAtEnd(head, 17);
+	insertAtEnd(head, 7);
+	insertAtEnd(head, 70);
+	insertAtEnd(head, 20);
+
+	print(head);
+
+  ReverseLL(head);
+  print(head);
+
+	
+	node* ans = mid(head);
+	cout << "Middle element is " << ans->data << endl;
+
+	
+	return 0;
+}
