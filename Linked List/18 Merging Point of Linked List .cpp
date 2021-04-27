8081d939ea2796bf362bcd88b2175703d4b9f024
Tцѕндя Kцкяд <43209472+TusharKukra@users.noreply.github.com>
2021-04-27 21:19:29 +0530

    Update 18 Merging Point of Linked List .cpp

--- Linked List/18 Merging Point of Linked List .cpp
+++ Linked List/18 Merging Point of Linked List .cpp
@@ -1,1 +1,13 @@
-
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL){
+            return NULL;
+        }
+        ListNode* a = headA;
+        ListNode* b = headB;
+        
+        while(a!=b){
+            a = a==NULL ? headB : a->next;
+            b = b==NULL ? headA : b->next;
+        }
+        return a;
+    }
