06009d293dc180d033d3a940ed3c77ffadeac431
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-12 19:41:04 +0530

    Create LeetCode- Sorted-Squares .cpp

---
+++ LeetCode- Sorted-Squares .cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// LeetCode : Sorted-Squares
+
+// Given a non-descending array : u have to square all the elements and sort the array
+// Element can be positive or negative
+
+// Approach : 
+
+/*
+Initially we have 2 parts of array : +Ve part & -Ve part
+then after doing square of both types of elements we have <0 value on 1st half and >0 on 2nd half
+then we merge both part of array into 1 sorted array
+*/
+
