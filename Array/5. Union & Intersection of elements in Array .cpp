c34742590c1e735314df9aac0dc94ab739df21cd
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-14 17:12:27 +0530

    Update 5. Union & Intersection of elements in Array .cpp

--- Array/5. Union & Intersection of elements in Array .cpp
+++ Array/5. Union & Intersection of elements in Array .cpp
@@ -62,3 +62,65 @@ int main(){
     unionOfArrays(a,b,n,m);
     return 0;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Method -2 (Using Set)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+Union of Two Arrays 
+
+Approach : Using Sets
+*/
+
+void unionOfArrays(int* a, int* b, int n, int m){
+
+    //create set container 
+    set<int> s;
+
+    // insert elements of both arrays into set
+    // 1. for array a
+    for(int i=0;i<n;i++){
+        s.insert(a[i]);
+    } 
+
+    // 2. for array b
+    for(int j=0;j<m;j++){
+        s.insert(b[j]);
+    } 
+
+    // Set will automatically remove duplicates from both arrays
+
+    // for printing: we use iterator
+
+    for(auto it = s.begin(); it!=s.end();it++){
+        cout<<*it<<" ";
+    }
+
+    // for printing number of elements in the final set
+    cout<<endl;
+    cout<<"Total no. of elements after union : "<<s.size();
+
+}
+
+int main(){
+
+    int n,m;
+    cin>>n;
+    int* a = new int[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cin>>m;
+    int* b = new int[m];
+    for(int j=0;j<m;j++){
+        cin>>b[j];
+    }
+
+    unionOfArrays(a,b,n,m);
+    return 0;
+}
