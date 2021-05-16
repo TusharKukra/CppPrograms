1ab2cce10a7a75e5824d1379e4ac0f83477d20e5
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-16 20:28:27 +0530

    Update Stack -1 Next Largest Element (to the Right) .cpp

--- Stack/Stack -1 Next Largest Element (to the Right) .cpp
+++ Stack/Stack -1 Next Largest Element (to the Right) .cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/*
+Stack --Next Largest Element (Nearest Greater to Right) Problem : 
 
+Problem: 
+    We have given an array[] = 1 3 2 4
+    Now we have to find greater element just right to the key element
+
+    Let in above array we have given key = 3 now greater than 3 is 4 on its right side of array
+
+    Or if we dont have any key, then we can find NGR of all the elements of the array
+
+Approach:
+    Brute Force : O(n^2)
+    Stack Approach : O(n)
+
+    Using Stack :
+        
+*/
