e3ba53b5e62c7f156f1a7e4e95c44b9406c7e1e4
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-21 20:48:34 +0530

    Update 2. Painter's Partition Problem .cpp

--- Binary Search/2. Painter's Partition Problem .cpp
+++ Binary Search/2. Painter's Partition Problem .cpp
@@ -25,3 +25,68 @@ Sample Output
 
 // Code :
 
+    #include<bits/stdc++.h>
+    using namespace std;
+
+    /*
+    Aggressive Cow 
+    */
+int numberOfPaintersRequierd(int N, int* boardLengths, int timeLimit){
+    int numPainter = 1; // intitially i have only 1 painter
+    int time= 0;
+
+    for(int i=0;i<N;i++){
+        time+=boardLengths[i];
+        if(time > timeLimit){
+            numPainter++;
+            time = boardLengths[i];
+        }
+    }
+    return numPainter;
+
+}
+
+int minTimeToPaint(int K, int N, int* boardLengths){
+
+    int sum =0;
+    int maxLen = 0;
+    for(int i=0;i<N;i++){
+        sum+= boardLengths[i];
+        maxLen = max(maxLen, boardLengths[i]);
+    }
+
+    int s= maxLen;
+    int e = sum;
+    int ans;
+
+    while(s<=e){
+        int m = s+ (e-s)/2;
+        if(numberOfPaintersRequierd(N,boardLengths,m) == K){
+            // k painters can paint N boards in 'm' amount of time
+            ans = m;
+            e = m-1;
+        }
+        else{
+            s = m+1;
+        }
+    }
+    return ans;
+}
+
+    int main(){
+
+            int K;
+            cin>>K;
+            
+            int N;
+            cin>>N;
+
+            int* boardLengths = new int[K];
+            for(int i=0;i<K;i++){
+                cin>>boardLengths[i];
+            }
+            sort(boardLengths, boardLengths+K);
+            cout<<minTimeToPaint(K,N,boardLengths)<<endl;
+        
+        return 0;
+    }
