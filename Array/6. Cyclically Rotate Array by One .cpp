64a5aeedf91b36eacb0fdac17d9d52d3cc997be8
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-23 09:03:30 +0530

    Update 6. Cyclically Rotate Array by One .cpp

--- Array/6. Cyclically Rotate Array by One .cpp
+++ Array/6. Cyclically Rotate Array by One .cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+// Array Rotation Cyclically
+
+
+void rotateTheArrayByOne(int* arr, int n){
+
+    int temp = arr[n-1]; // store the last element
+
+    // now shift remaining array 1 step ahead
+    for(int i = n-1 ; i>0 ;i--){
+        arr[i] = arr[i-1];  // i.e the element at at (i-1)th position will be shifted to the ith position : ((i-1) + 1)th 
+    }
+
+    // the last element comes to the front
+    arr[0] = temp;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int* arr = new int[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<endl;
+    rotateTheArrayByOne(arr,n);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
