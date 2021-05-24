9e91e8c5cf6d156afd809b98cdd60503ff755563
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-24 20:14:39 +0530

    Create 4. Min abs Diff two Unsorted Arrays .cpp

---
+++ Binary Search/4. Min abs Diff two Unsorted Arrays .cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Min Abs Diff Two Unsorted Arrays
+
+/* 
+Given two arrays of int, compute the pair of values (one value in each array)
+with the smallest non-neg diff . Return the difference.
+
+Input : A[] {1,3,15,11,2}
+		B[] {23, 127, 235, 19, 8}
+
+Output : 3
+That is, the pair (11,8)
+*/
+
+
+
+int main(){
+
+	
+	return 0;
+}
