ebb6babcf76aa935e8fb7b265017d730010e3472
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-28 20:28:56 +0530

    Update 7. Redundant Parentheses .cpp

--- Stack/7. Redundant Parentheses .cpp
+++ Stack/7. Redundant Parentheses .cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+/* 
+Given a string of balanced expression, find if it contains a redundant parentheses or not.
+
+A set of parentheses are redundant if same sub-expression is surrounded by unnecessary or multiple brackets. Print 'Yes' if redundant else 'No'.
+
+Note: Expression may contain '+', '*', '-' and '/' operators. Given expression is valid and there are no white spaces present.
+
+Ex : 
+Input : ((a+b))  Output : Yes
+Input : (a+(b)/c)  Output : Yes
+Input : (a+b*(c-d))  Output : No
+
+*/
+
+bool isRedundant(string expr){
+
+
+}
+
+int main(){
+
+    return 0;
+}
