239c84852d6a8fa93204fa6848a146660689a34d
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-28 22:26:14 +0530

    Update 7. Redundant Parentheses .cpp

--- Stack/7. Redundant Parentheses .cpp
+++ Stack/7. Redundant Parentheses .cpp
@@ -16,11 +16,34 @@ Input : (a+b*(c-d))  Output : No
 */
 
 bool isRedundant(string expr){
+    stack<char> stack;
+    for(char ch : expr){
+        if(ch == ')'){
+            bool opFound = false;
+            while(stack.top() != '('){
+                char top = stack.top();
+                stack.pop();
+                if(top=='+' || top=='*' || top=='-' || top=='/')
+                    opFound = true;
+            }
 
-
+            stack.pop(); // pop the opening bracket
+            if(!opFound) return true;
+        }
+        else{
+            stack.push(ch);
+        }
+    }
+    return false;
 }
 
 int main(){
+    string expr ("(a+b*(c-d))");
 
+    // remove whitespaces
+    expr.erase(remove(expr.begin(), expr.end(), ' '), expr.end());
+    cout<<expr<<endl;
+
+    isRedundant(expr) ? cout<<"Yes"<<endl : cout<<"No"<<endl;
     return 0;
 }
