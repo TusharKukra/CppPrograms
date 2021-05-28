19377b473b63ca415ee0f1b8016b48a23d8c8a73
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-05-28 09:40:29 +0530

    Update 2. Checking for Duplicate Characters in String .cpp

--- String/2. Checking for Duplicate Characters in String .cpp
+++ String/2. Checking for Duplicate Characters in String .cpp
@@ -30,29 +30,30 @@ int main(){
 #include<bits/stdc++.h>
 using namespace std;
 
-// Duplicate characters in a String (using Hashing (maps))
-void checkDuplicate(string a){
-    
-    map <char, int> cnt;
-    for(int i=0; i<a.length();i++){
-        cnt[a[i]]++;  // for every character in a string store its freq value in count
+// Check Duplicate Elements : Better Approach (using Hash Map)
+
+void checkDuplicates(string s){
+
+    map<char, int> count;
+    for(int i=0;i<s.length();i++){
+        count[s[i]]++;  // firstly we store each and every character value into the count map
     }
 
-    // then check for duplicate element : if for any character count value > 1 (i.e it is repeated character)
-    for(auto it : cnt){
-        if(it.second>1){
-            cout<<it.first<<" : "<<it.second<<" times"<<endl;
+    // now we traverse the map and look for the count value which is greater than 1 (i.e which character occurs more than once)
+    for(auto it : count){
+        if(it.second >1){  // it.second means our map stores 2 value : character and its integer count so we need integer value to check its occurrence.
+            cout<<it.first<<" occurs : "<<it.second<<" times."<<endl;
         }
     }
+
 }
 
 int main(){
 
-    string a;
-    getline(cin,a);
+    string s;
+    getline(cin,s);
 
-    checkDuplicate(a);
+    checkDuplicates(s);
 
     return 0;
-
 }
