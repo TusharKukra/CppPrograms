0392655bf6d206c7fecf88c7b6ffed7c22792d31
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-06-06 07:26:41 +0530

    Update Max Subarray Sum (Kadane Algo) .cpp

--- Array/Max Subarray Sum (Kadane Algo) .cpp
+++ Array/Max Subarray Sum (Kadane Algo) .cpp
@@ -4,31 +4,33 @@ using namespace std;
 // Maximum Subarray Sum using Kadane Algorithm
 // Time Complexity : O(n)
 
-int main(){
 
-     int n;
-     cin>>n;
+int kadane(int* a, int n){
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+    
+    for(int i=0;i<n;i++){
+        currentSum = currentSum + a[i];
+        if(currentSum<0){
+            currentSum = 0;
+            }
+        maxSum = max(currentSum, maxSum);
+    }
 
-     int a[n], currentSum =0, maxSum=0;
+    return maxSum;
+}
 
-     for(int i=0;i<n;i++){
-          cin>>a[i];
-     }
+int main(){ 
 
+    int n;
+    cin>>n;
 
-     //Kadane Algo:
-     for(int i=0;i<n;i++){
-          currentSum += a[i];
+    int *a = new int[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
 
-          if(currentSum < 0){
-               currentSum = 0;
-          }
+    cout<<kadane(a,n)<<endl;
 
-          maxSum = max(currentSum,maxSum);
-     }
-
-     cout<<"Maximum Sum : "<<maxSum<<endl;
-
-
-     return 0;
+    return 0;
 }
