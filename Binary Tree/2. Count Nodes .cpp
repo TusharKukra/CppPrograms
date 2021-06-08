37380d69351e4b4312038c29d0c6b77caf1582c0
Tushar Kukra <43209472+TusharKukra@users.noreply.github.com>
2021-06-08 20:39:52 +0530

    Create 2. Count Nodes .cpp

---
+++ Binary Tree/2. Count Nodes .cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Binary Tree
+
+class node{
+
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        data = d;
+        left = right = NULL;
+    }
+
+};
+
+// create Tree
+node* createTree(){
+
+    // enter data
+    int data;
+    cin>>data;
+    
+    if(data == -1){  // if tree is empty
+        return NULL;
+    }
+
+    // create node
+    node* root = new node(data);
+
+    // now first create left subtree  (using recursion)
+    root->left = createTree();
+
+    // now create right subtree  (using recursion)
+    root->right = createTree();
+}
+
+void preOrder(node* root){
+    // base
+    if(root == NULL){
+        return;
+    }
+
+    // root left right
+    cout<<root->data<<" ";
+    preOrder(root-> left);
+    preOrder(root->right);
+
+}
+
+void inOrder(node* root){
+    // base
+    if(root == NULL){
+        return;
+    }
+
+    // left root right
+    preOrder(root-> left);
+    cout<<root->data<<" ";
+    preOrder(root->right);
+
+}
+
+void postOrder(node* root){
+    // base
+    if(root == NULL){
+        return;
+    }
+
+    // left right root
+    preOrder(root-> left);
+    preOrder(root->right);
+    cout<<root->data<<" ";
+
+
+}
+
+////////////////////////////////////////////////////////////////////
+
+// Count Nodes : 
+int countNodes(node* root){
+
+    if(root==NULL){
+        return 0;
+    }
+
+    return countNodes(root->left) + countNodes(root->right) + 1;  // Post Order : Left Right Root
+} 
+
+////////////////////////////////////////////////////////////////////
+
+
+int main(){
+
+    node* root = NULL;
+
+    root = createTree();
+
+    preOrder(root);
+    cout<<endl;
+
+    postOrder(root);
+    cout<<endl;
+
+    inOrder(root);
+    cout<<endl;
+
+    cout<<countNodes(root)<<endl;
+
+    return 0;
+}
